87e6c1591f725cb47bd3202dcb71399ceeea4565
JuanMiguel <juan.noriega@correounivalle.edu.co>
2024-03-12 18:06:31 -0500

    Se suben los archivos base del microservicio de tecnologia

---
+++ src/main/java/com/pragma/technologymicroservice/TechnologyMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.technologymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnologyMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/technologymicroservice/TechnologyMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.technologymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologyMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
