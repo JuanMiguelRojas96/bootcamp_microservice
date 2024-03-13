91a4bea2a4dee93ceb4bc50262f40736496d8dfb
Juan Miguel Rojas <127504148+JuanMiguelRojas96@users.noreply.github.com>
2024-03-12 20:42:57 -0500

    Revert "Se agrega la capa de Dominio, teniendo en cuenta el crear tecnologia"

--- src/main/java/com/pragma/technologymicroservice/domain/exception/EmptyFieldException.java
+++
@@ -1,7 +0,0 @@
-package com.pragma.technologymicroservice.domain.exception;
-
-public class EmptyFieldException extends RuntimeException{
-  public EmptyFieldException(String message){
-    super(message);
-  }
-}
