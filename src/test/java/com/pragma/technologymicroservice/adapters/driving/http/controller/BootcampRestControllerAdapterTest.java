95af3aeb3128d899ca72ee568a63854582ee3fda
JuanMiguel <juan.noriega@correounivalle.edu.co>
2024-04-01 12:07:23 -0500

    Se terminan los Test Unitarios de la HU6

--- src/test/java/com/pragma/technologymicroservice/adapters/driving/http/controller/BootcampRestControllerAdapterTest.java
+++ src/test/java/com/pragma/technologymicroservice/adapters/driving/http/controller/BootcampRestControllerAdapterTest.java
@@ -1,6 +1,8 @@
 package com.pragma.technologymicroservice.adapters.driving.http.controller;
 
 import com.pragma.technologymicroservice.adapters.driving.http.dto.request.AddBootcampRequest;
+import com.pragma.technologymicroservice.adapters.driving.http.dto.response.BootcampResponse;
+import com.pragma.technologymicroservice.adapters.driving.http.dto.response.CapacityDto;
 import com.pragma.technologymicroservice.adapters.driving.http.mapper.IBootcampRequestMapper;
 import com.pragma.technologymicroservice.adapters.driving.http.mapper.IBootcampResponseMapper;
 import com.pragma.technologymicroservice.domain.api.IBootcampServicePort;
@@ -14,6 +16,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -48,6 +52,29 @@ class BootcampRestControllerAdapterTest {
 
     assertEquals(HttpStatus.CREATED,response.getStatusCode());
     verify(bootcampServicePort, times(1)).saveBootcamp(bootcamp);
+
+  }
+
+  @Test
+  void testGetAllBootcamps(){
+
+    List<CapacityDto> capacities = new ArrayList<>();
+    capacities.add(new CapacityDto(1L,"Capacidad",List.of()));
+    capacities.add(new CapacityDto(2L,"Capacidad",List.of()));
+
+    List<BootcampResponse> responseList = Collections.singletonList(new BootcampResponse(1L,"Bootcamp","descripcion",capacities));
+
+    when(bootcampServicePort.getAllBootcamps(1,10,true,true)).thenReturn(Collections.emptyList());
+    when(bootcampResponseMapper.toBootcampResponseList(Collections.emptyList())).thenReturn(responseList);
+
+    ResponseEntity<List<BootcampResponse>> responseEntity = bootcampRestControllerAdapter.getAllBootcamps(1,10,true,true);
+
+    assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
+    assertEquals(responseList,responseEntity.getBody());
+
+    verify(bootcampServicePort, times(1)).getAllBootcamps(1,10,true,true);
+    verify(bootcampResponseMapper,times(1)).toBootcampResponseList(Collections.emptyList());
+
 
   }
 
